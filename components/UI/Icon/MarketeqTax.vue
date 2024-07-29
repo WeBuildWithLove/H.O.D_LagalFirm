0adb31f1294c26f9f344b639dddd80cdbad83126
kufre-abasi <kufreabasibassey3@gmail.com>
2024-07-29 16:40:58 +0100

    docs: Icons

---
+++ components/UI/Icon/MarketeqTax.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg
+    width="50"
+    height="50"
+    viewBox="0 0 50 50"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M43.749 12.5L31.249 25M29.166 35.417H14.582M22.916 27.083H14.582"
+      stroke="#A70314"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M32.1875 13.542H32.3955M42.6035 23.958H42.8125"
+      stroke="#A70314"
+      stroke-width="3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M37.5 33.333V41.667C37.5 42.2194 37.2805 42.7493 36.8899 43.1399C36.4993 43.5305 35.9694 43.75 35.417 43.75H8.333C7.78055 43.75 7.25073 43.5305 6.8601 43.1399C6.46946 42.7493 6.25 42.2194 6.25 41.667V8.333C6.25 7.78055 6.46946 7.25073 6.8601 6.8601C7.25073 6.46946 7.78055 6.25 8.333 6.25H22.917"
+      stroke="#A70314"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
